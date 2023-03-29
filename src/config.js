517bc43c6c9311766ddabf2468d599074f5d7a10
Bowie <lfranckx@thinkful.com>
2023-03-29 13:41:31 -0500

    modify api endpoint for deployed server address

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-    API_ENDPOINT: "http://localhost:8000",
+    API_ENDPOINT: "https://dry-gorge-20170.herokuapp.com",
+    // API_ENDPOINT: "http://localhost:8000",
     TOKEN_KEY: 'JWT'
 }
